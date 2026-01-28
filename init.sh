97ca3a34efdf65bcdf85e2b4470b6b21fd534093
Roberto Gritti <roberto.gritti@gmail.com>
2026-01-29 00:06:38 +0100

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt-get -y install podman samba
+
+mkdir /media/ext
+echo -e "\n/dev/sda1       /media/ext      exfat   auto" >> /etc/fstab
+mount -a
+mkdir /media/ext/media/movies
+mkdir /media/ext/media/series
+
+echo -e "\n[plexmedia]" >> /etc/samba/smb.conf
+echo -e "\n    comment = Plex media folder"
+echo -e "\n    path = /media/ext/media"
+echo -e "\n    read only = no"
+echo -e "\n    browsable = yes\n"
+sudo service smbd restart
+sudo ufw allow samba
+
